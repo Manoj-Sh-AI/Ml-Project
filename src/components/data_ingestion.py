5294aacde1a1f35156b9fd484c024d8c0ac9927f
Manoj-Sh-AI <shmanoj100@gmail.com>
2023-09-01 01:12:15 +0530

    Commit-5: Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 from exception import CustomException
 from logger import logging
+from data_transformation import DataTransformation, DataTransformationConfig
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -54,9 +55,10 @@ class DataIngestion: # contains required functions
         
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
 
 
