2f6c11c6d224a9fe3a5e887d3594e77874a32fa5
Manoj-Sh-AI <shmanoj100@gmail.com>
2025-12-08 01:36:20 +0100

    Final Commit

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -35,31 +36,24 @@ class CustomData:
         reading_score: int,
         writing_score: int,
     ):
-
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
+                "race/ethnicity": [self.race_ethnicity],
+                "parental level of education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+                "test preparation course": [self.test_preparation_course],
+                "reading score": [self.reading_score],
+                "writing score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
